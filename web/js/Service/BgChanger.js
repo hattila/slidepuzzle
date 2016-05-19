567d98df9ba5b96f3504cc84243c006e582a6faa
Héjjas Attila <attila.h9@gmail.com>
2016-05-19 20:25:45 +0200

    add optional new backgrounds.

---
+++ web/js/Service/BgChanger.js
@@ -0,0 +1,35 @@
+/**
+ * Created by hattila on 2016.05.19..
+ */
+
+Hw.Srvc.BgChanger = Hw.Srvc.BgChanger || (function(){
+
+    var _bg = 0;
+    var _bgs = [
+        '../image/forest-patrol-flat-1600.png',
+        '../image/forest_1600.png',
+        '../image/mountains_1600.jpg',
+        '../image/doom_logo_1600.jpg'
+    ];
+
+    var init = function () {
+        _showBgSelection();
+        _addClickHandlers();
+    };
+
+    var _showBgSelection = function () {
+        
+    };
+    
+    var _addClickHandlers = function () {
+        
+    };
+
+    var _changeTilesBackground = function (bg) {
+        
+    };
+
+    return {
+        init: init
+    }
+})();
